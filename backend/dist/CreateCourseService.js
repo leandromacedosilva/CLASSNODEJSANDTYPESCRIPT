1ef7d9281489c5a80b0ed3265f5bdf1b746472a0
leandromacedosilva <iamfulldeveloper@gmail.com>
2022-07-24 12:07:43 -0300

    updated files in project

--- backend/dist/CreateCourseService.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class CreateCouseService {
-    execute(name, duration, educator) {
-        console.log(name, duration, educator);
-    }
-}
-exports.default = new CreateCouseService();
